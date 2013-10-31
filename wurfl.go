7673533e3c1758420640cf9000608126ac48e6e2
Damian Gryski <damian@gryski.com>
2013-10-31 21:16:03 +0100

    support loading xml patch files

--- wurfl.go
+++ wurfl.go
@@ -20,15 +20,28 @@ type Wurfl struct {
 	mu    sync.Mutex
 }
 
-func New(wurflxml string) (*Wurfl, error) {
+func New(wurflxml string, patches ...string) (*Wurfl, error) {
 	w := &Wurfl{}
 
 	w.wurfl = C.wurfl_create()
-	C.wurfl_set_engine_target(w.wurfl, C.WURFL_ENGINE_TARGET_HIGH_PERFORMANCE)
+	C.wurfl_set_engine_target(w.wurfl, C.WURFL_ENGINE_TARGET_HIGH_ACCURACY)
 	C.wurfl_set_cache_provider(w.wurfl, C.WURFL_CACHE_PROVIDER_DOUBLE_LRU, C.CString("10000,3000"))
 	wxml := C.CString(wurflxml)
 	C.wurfl_set_root(w.wurfl, wxml)
-	C.free(unsafe.Pointer(wxml))
+
+	var freeme []*C.char
+	defer func() {
+		C.free(unsafe.Pointer(wxml))
+		for _, f := range freeme {
+			C.free(unsafe.Pointer(f))
+		}
+	}()
+
+	for _, pxml := range patches {
+		p := C.CString(pxml)
+		freeme = append(freeme, p)
+		C.wurfl_add_patch(w.wurfl, p)
+	}
 
 	if C.wurfl_load(w.wurfl) != C.WURFL_OK {
 		err := C.wurfl_get_error_message(w.wurfl)
